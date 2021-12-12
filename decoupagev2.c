343009d368bfcfe16efb05c7f2dbe244857d10d8
Yvan <contact@yvan.dev>
2021-12-12 16:29:51 +0100

    changement identation

--- decoupagev2.c
+++ decoupagev2.c
@@ -7,74 +7,74 @@
 
 void printcase(int casevoulue){
 
-	couleur coul;
+    couleur coul;
 
-	coul=CouleurParNom("black");
-	ChoisirCouleurDessin(coul);
+    coul=CouleurParNom("black");
+    ChoisirCouleurDessin(coul);
 
- int hauteurimage, largeurimage, lignevoulue, colonnevoulue;
- int choiximage;
- int decoupageligne, decoupagecolonne;
-  choiximage=2;
-  decoupageligne=4;
-  decoupagecolonne=4;
-  int bouclehauteur=0, bouclelargeur=0;
-  int decalage=0,decalagehauteur=0;
+    int hauteurimage, largeurimage, lignevoulue, colonnevoulue;
+    int choiximage;
+    int decoupageligne, decoupagecolonne;
+    choiximage=2;
+    decoupageligne=4;
+    decoupagecolonne=4;
+    int bouclehauteur=0, bouclelargeur=0;
+    int decalage=0,decalagehauteur=0;
 
-  int hauteurimagecree=0, largeurimagecree=0;
+    int hauteurimagecree=0, largeurimagecree=0;
 
-  int lignecase=0;
+    int lignecase=0;
 
-  int x,y;
-  int largeurcasecree, hauteurcasecree;
+    int x,y;
+    int largeurcasecree, hauteurcasecree;
 
-  int suite[8][8];
-  int compteursuite=0;
-  
-  ChoisirEcran(3);
+    int suite[8][8];
+    int compteursuite=0;
 
-  EffacerEcran(coul);
+    ChoisirEcran(3);
 
-  if (choiximage==1){
-    largeurimage=600;
-    hauteurimage=399;
-    ChargerImage("./iut.png",0,0,0,0,600,399);
-  }
+    EffacerEcran(coul);
 
-   if (choiximage==2){
-    largeurimage=480;
-    hauteurimage=359;
-    ChargerImage("./victorydenis.png",0,0,0,0,480,359);
-  }
+    if (choiximage==1){
+        largeurimage=600;
+        hauteurimage=399;
+        ChargerImage("./iut.png",0,0,0,0,600,399);
+    }
 
-   /* autres choix image */
+    if (choiximage==2){
+        largeurimage=480;
+        hauteurimage=359;
+        ChargerImage("./victorydenis.png",0,0,0,0,480,359);
+    }
 
-   for (x=0; x<decoupageligne; x++){
-     for (y=0; y<decoupagecolonne; y++){
-       
-     lignecase=0;
+    /* autres choix image */
 
-     /* à utiliser :  */
-     /* decoupagecolonne / decoupageligne / suite */
-     
-     casevoulue=suite[x][y]; /* à intégrer avec fonction random */
+    for (x=0; x<decoupageligne; x++){
+        for (y=0; y<decoupagecolonne; y++){
 
-     /* pour déroulé jeu : possibilité de reparcourir ce code avec un "cache" pour ne charger l'image que quand il y a besoin d'un changement */
+        lignecase=0;
 
-       printf("%d ", casevoulue); /* pour vérif */
-   while(casevoulue>=decoupagecolonne){
-     casevoulue=casevoulue-decoupagecolonne;
-     lignecase++;
-   }
+        /* à utiliser :  */
+        /* decoupagecolonne / decoupageligne / suite */
 
-   largeurcasecree=largeurimage/decoupagecolonne*casevoulue; 
-   hauteurcasecree=hauteurimage/decoupageligne*lignecase;
-   if (casevoulue != 0 && lignecase != 0){
-      ChargerImage("./victorydenis.png",700+largeurimagecree+decalage,hauteurimagecree+decalagehauteur,largeurcasecree, hauteurcasecree, largeurimage/decoupagecolonne, hauteurimage/decoupageligne);
-      largeurimagecree=largeurimagecree+(largeurimage/decoupagecolonne);
-   }
-      decalage=decalage+5;
-     }
+        casevoulue=suite[x][y]; /* à intégrer avec fonction random */
+
+        /* pour déroulé jeu : possibilité de reparcourir ce code avec un "cache" pour ne charger l'image que quand il y a besoin d'un changement */
+
+        printf("%d ", casevoulue); /* pour vérif */
+        while(casevoulue>=decoupagecolonne){
+            casevoulue=casevoulue-decoupagecolonne;
+            lignecase++;
+        }
+
+        largeurcasecree=largeurimage/decoupagecolonne*casevoulue; 
+        hauteurcasecree=hauteurimage/decoupageligne*lignecase;
+        if (casevoulue != 0 && lignecase != 0){
+            ChargerImage("./victorydenis.png",700+largeurimagecree+decalage,hauteurimagecree+decalagehauteur,largeurcasecree, hauteurcasecree, largeurimage/decoupagecolonne, hauteurimage/decoupageligne);
+            largeurimagecree=largeurimagecree+(largeurimage/decoupagecolonne);
+        }
+        decalage=decalage+5;
+        }
     hauteurimagecree=hauteurimagecree+(hauteurimage/decoupageligne);
     largeurimagecree=0;
     bouclelargeur=0;
@@ -82,15 +82,15 @@ void printcase(int casevoulue){
     decalage=0;
     decalagehauteur=decalagehauteur+5;
     printf("\n"); /* pour vérif */
- }
 
-  
-  CopierZone(3,0,0,0,LONGFENETR,HAUTFENETR,0,0);
+    }
 
-  /* gestion des coordonnées */
+    CopierZone(3,0,0,0,LONGFENETR,HAUTFENETR,0,0);
 
-  Touche();
-  
- 
+    /* gestion des coordonnées */
+
+    Touche();
+
+
 }
 
